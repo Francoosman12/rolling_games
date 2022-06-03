cedbc5880828c68b75b37aead062f4bee9856534
francoosman12 <francoosman12@gmail.com>
2022-06-02 21:40:33 -0300

    Soluciones las flechas de las categorias

--- js/juegos.js
+++ js/juegos.js
@@ -37,16 +37,63 @@ let juegos = [
   },
   {
     id: "5",
-    categoria: "",
-    nombre: "",
+    categoria: "accion",
+    nombre: "Tomb Rider",
     descripcion: "",
-    image: "",
+    image: "/assets/img/5.jpg",
+    video: "",
+    destacado: "",
+  },
+  {
+    id: "6",
+    categoria: "aventura",
+    nombre: "Uncharted 4",
+    descripcion: "",
+    image: "/assets/img/6.png",
+    video: "",
+    destacado: "",
+  },
+  {
+    id: "7",
+    categoria: "aventura",
+    nombre: "Rayman",
+    descripcion: "",
+    image: "/assets/img/7.jpg",
+    video: "",
+    destacado: "",
+  },
+  {
+    id: "8",
+    categoria: "aventura",
+    nombre: "Horizon",
+    descripcion: "",
+    image: "/assets/img/8.jpeg",
+    video: "",
+    destacado: "",
+  },
+  {
+    id: "9",
+    categoria: "aventura",
+    nombre: "Watch Dogs 2",
+    descripcion: "",
+    image: "/assets/img/9.png",
+    video: "",
+    destacado: "",
+  },
+  {
+    id: "10",
+    categoria: "aventura",
+    nombre: "Assassin's Creed",
+    descripcion: "",
+    image: "/assets/img/10.jpg",
     video: "",
     destacado: "",
   },
 ];
 
 //CARRUSEL
+
+//ACCION
 
 const accionCarrousel = document.getElementById("accion-carrousel");
 const accionJuegos = juegos.filter((juego) => juego.categoria == "accion");
@@ -63,4 +110,23 @@ accionJuegos.forEach((juego) => {
     </div>
     `;
   accionCarrousel.appendChild(card);
+});
+
+//AVENTURA
+
+const aventuraCarrousel = document.getElementById("aventura-carrousel");
+const aventuraJuegos = juegos.filter((juego) => juego.categoria == "aventura");
+
+aventuraJuegos.forEach((juego) => {
+  let card = document.createElement("div");
+  card.innerHTML = `
+    <div class='card' id=${juego.id}>
+    <img src=${juego.image} class="card-img" alt="#">
+    <div class= "card-body">
+         <h2 class= "juego-tittle">${juego.nombre}</h2>
+         <button class="verJuego-btn"><a href=/detalle.html#${juego.id}>Ver</a></button>
+    </div>
+    </div>
+    `;
+  aventuraCarrousel.appendChild(card);
 });

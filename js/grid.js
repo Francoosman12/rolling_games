044635f85dc2aac9fb3afafd62d0ee04e40ee118
mat <matbarahona71@gmail.com>
2022-06-06 22:15:18 -0300

    cargar juegos desde localstorage

--- js/grid.js
+++ js/grid.js
@@ -1,7 +1,8 @@
+let juegos = JSON.parse(localStorage.getItem("juegos")) || [];
 let generos = ["Action","Horror","Fighting","Action RPG","RPG","FPS","Flight","Shooter"]
 
 
-// let juegos = JSON.parse(localStorage.getItem("juegos")) || [];
+
 let gameContainer = document.getElementById("gameContainer")
 
 const loadGames=()=>{    
@@ -10,13 +11,15 @@ const loadGames=()=>{
         let div=document.createElement('div')
         div.setAttribute("class","col-6 col-md-3 col-lg-2")        
         let cardContent=`<div class="card border-0">
-                    <a onclick="mostrarDetalles(${index})" href="../pages/gameDetails.html">
+                    <a class="game" onclick="mostrarDetalles(${index})" href="../pages/gameDetails.html">
                     <img src="${juego.poster}" alt="">
-                    </a>
-                    <div class="card-body p-1"></div>
-                        <h5 class="card-title">${juego.title}</h5>
-                        <p class="card-text genre">${generos}</p>
-                        <p class="card-text">$${juego.price||59.99}</p>
+                    </a>                    
+                    <div class="card-body p-1">
+                    <h5 title="${juego.title}" class="card-title">${juego.title}</h5>
+                    <span class="card-text genre">${generos}</p>
+                    <span class="card-text">$${juego.price||59.99}</p>
+                    </div>
+                    
                 </div>`
         div.innerHTML=cardContent
         gameContainer.appendChild(div)

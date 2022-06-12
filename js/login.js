5d74835c7ecfaa6da644f1633f19c37da5ca1dcf
artreve <sebas981@live.com>
2022-06-12 19:56:35 -0300

    Logica de login lista, junto con el diseño de los modales

--- js/login.js
+++ js/login.js
@@ -10,41 +10,41 @@ function handleSubmit(e){
     if (validacion){
             if(validacion.password === pass){
                 //guardamos la sesion del usuario
-                // var modal=document.getElementById("modal");
-                // modal.style.display="none";
-                localStorage.setItem('user',JSON.stringify(validacion));
+                localStorage.setItem('userLogin',JSON.stringify(validacion));
                 
                 let listContainer = document.getElementById('listaMenu');
                 //reemplazar
-                // let login = document.getElementById('loginNav');
-                // let registro = document.getElementById('regNav');
+                let login = document.getElementById('loginNav');
+                let registro = document.getElementById('regNav');
+                login.style.display = 'none';
+                registro.style.display = 'none';
                 // console.log(login);
                 // console.log(registro);
                 // document.ul.removeChild(login);
                 // document.ul.removeChild(registro);
-
-                // let btnUser = document.createElement('li');
-                // btnUser.classList = "nav-item";
-                // let optionUser = `<a class="nav-link botones active"
-                // href="#">Hola ${validacion.nombre}</a>`;
-                // btnUser.innerHTML = optionUser;
-                // listContainer.appendChild(btnUser);
-
+                
+                // Opciones de Usuario
                 let btnUser = document.createElement('li');
-                btnUser.classList = "nav-item dropdown";
-                let optionUser = `<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                hola ${validacion.nombre}
-              </a>
-              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <li><a class="dropdown-item" href="#">Action</a></li>
-                <li><a class="dropdown-item" href="#">Another action</a></li>
-                <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#">Something else here</a></li>
-              </ul>`;
+                btnUser.classList = "nav-item";
+                let optionUser = `<a class="nav-link botones active"
+                href="#" data-bs-toggle="modal" data-bs-target="#cerrarSesion">Hola ${validacion.nombre}</a>`;
                 btnUser.innerHTML = optionUser;
                 listContainer.appendChild(btnUser);
 
-                // document.body.removeChild(imagen);
+              //   let btnUser = document.createElement('li');
+              //   btnUser.classList = "nav-item dropdown";
+              //   let optionUser = `<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              //   hola ${validacion.nombre}
+              // </a>
+              // <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              //   <li><a class="dropdown-item" href="#">Action</a></li>
+              //   <li><a class="dropdown-item" href="#">Another action</a></li>
+              //   <li><hr class="dropdown-divider"></li>
+              //   <li><a class="dropdown-item" href="#">Something else here</a></li>
+              // </ul>`;
+              //   btnUser.innerHTML = optionUser;
+              //   listContainer.appendChild(btnUser);
+
                 if(validacion){
                     if(validacion.rol === "admin"){
                       let btnAdmin = document.createElement("li");
@@ -56,7 +56,6 @@ function handleSubmit(e){
                     }
                   }
                   
-                // location.replace('../index.html');
             }else{
                 alert('El correo o password son incorrectos');
             }
